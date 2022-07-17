e6b9ba68b20b45644754b9abbe9712ca35e5d504
Kelvin van Vuuren <kelvin.van-vuuren@crick.ac.uk>
2022-07-17 22:08:06 +0000

    exercise1-3 add header to temperature conversion program

---
+++ chapter1/exercise1-3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    float fahr, celsius;
+    int lower, upper, step;
+    
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    printf("Celsius  Fahrenheit\n");
+    
+    fahr = lower;
+    while (fahr <= upper) {
+        celsius = (5.0 / 9.0) * (fahr - 32.0);
+        printf("%7.0f %11.1f\n", celsius, fahr);
+        fahr = fahr + step;
+    }
+}
