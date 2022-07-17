5670d11fba463f9a5eafe854bd6440f268a26932
Kelvin van Vuuren <kelvin.van-vuuren@crick.ac.uk>
2022-07-17 22:25:42 +0000

    exercise1-5 temperature conversion program print table in reverse order

---
+++ chapter1/exercise1-5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    float fahr, celsius;
+    int lower, upper, step;
+    
+    lower = 0;
+    upper = 300;
+    step = 20;
+    
+    fahr = upper;
+    while (fahr >= lower) {
+        celsius = (5.0 / 9.0) * (fahr - 32.0);
+        printf("%3.0f %6.1f\n", celsius, fahr);
+        fahr = fahr - step;
+    }
+}
