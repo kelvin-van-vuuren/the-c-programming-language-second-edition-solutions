f42b348fd820f28a162e89ab5eda9bb97c353949
Kelvin van Vuuren <kelvin.van-vuuren@crick.ac.uk>
2022-07-22 17:09:42 +0000

    exercise1-14
    program to print a histogram of the frequencies of different characters in its input

---
+++ chapter1/exercise1-14.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#define N_ASCII_CHAR 256 /* Number of characters in extended ASCII character set */
+
+/* program to print a histogram of the frequencies of different characters in its input */
+
+int main()
+{
+    int c, i, j;
+    int frequencies[N_ASCII_CHAR];
+    
+    for (i = 0; i < N_ASCII_CHAR; ++i)
+        frequencies[i] = 0;
+    
+    while ((c = getchar()) != EOF)
+        ++frequencies['c'];
+    
+    printf(" ASCII code | frequency\n");
+    for (i = 0; i < N_ASCII_CHAR; ++i) {
+        printf("      %5d | ", i);
+        for (j = 0; j < frequencies[i]; ++j)
+            putchar('+');
+        putchar('\n');
+    }
+}
