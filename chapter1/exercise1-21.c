1236cf2301e844e6ac4c6766e58369a97f17e6cd
Kelvin van Vuuren <kelvin.van-vuuren@crick.ac.uk>
2022-08-03 21:05:01 +0000

    exercise1-21.c
    program that replaces strings of blanks with the minimum number of tabs and blanks to achieve the same spacing

---
+++ chapter1/exercise1-21.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#define TABSTOP 4
+
+/* program entab that replaces tabs in the input with the proper number of blanks to space to the next tab stop */
+
+int c, col, blanks;
+
+void entab();
+
+int main()
+{
+    col = blanks = 0;
+    while ((c = getchar()) != EOF) {
+        ++col;
+        entab();
+    }
+}
+
+void entab()
+{
+    int i, nb, nt;
+    
+    if (c == ' ') {
+        ++blanks;
+        if (col % TABSTOP == 0) {
+            nt = blanks / TABSTOP;
+            nb = blanks % TABSTOP;
+            for (i = 0; i < nt; ++i)
+                putchar('\t');
+            for (i = 0; i < nb; ++i)
+                putchar(' ');
+            blanks = 0;
+        }
+    } else {
+        putchar(c);
+        if (c == '\n')
+            col = 0;
+        else if (c == '\t')
+            col = col + (TABSTOP - (col - 1) % TABSTOP) - 1;
+    }
+}
