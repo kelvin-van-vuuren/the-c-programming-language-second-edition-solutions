c8d2ab422d3e3875694f64bd4dca0e0080abaea3
Kelvin van Vuuren <kelvin.van-vuuren@crick.ac.uk>
2022-08-09 22:00:27 +0000

    exercise2-5.c
    function any(s1, s2), which returns the first locaiton in the string s1 where any character from the string s2 occurs, or -1 if s1 contains no characters from s2

---
+++ chapter2/exercise2-5.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/* function any(s1, s2), which returns the first location in the string s1 where
+ * any character from the string s2 occurs, or -1 if s1 contains no characters
+ * from s2 */
+
+int any(char s1[], char s2[]);
+
+int main() 
+{
+    char s1[] = "hello";
+    char s2[] = "el";
+    printf("%d", any(s1, s2));
+}
+
+int any(char s1[], char s2[]) 
+{
+    int i, j;
+
+    for (i = 0; s1[i] != '\0'; ++i)
+        for (j = 0; s2[j] != '\0'; ++j) 
+            if (s1[i] == s2[j])
+                return i;
+                
+    return -1;
+}
