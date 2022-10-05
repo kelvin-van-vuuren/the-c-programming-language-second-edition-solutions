c487b29fc8540b76469e9072b7700a16631b75b6
Kelvin van Vuuren <kelvin.van-vuuren@crick.ac.uk>
2022-10-05 11:27:27 +0000

    exercise4-3.c
    extend polish notation calculator program to handle modulus operator and negative numbers

---
+++ chapter4/exercise4-3.c
@@ -0,0 +1,137 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/* extend polish notation calculator program to handle modulus operator and
+ * negative number */
+
+#define MAXOP 100  /* max size of operand or operator */
+#define NUMBER '0' /* signal that a number was found */
+
+#define MAXVAL 100 /* maximum depth of val stack */
+
+#define BUFSIZE 100 /* size of unread character array for ungetch */
+
+int getop(char s[]);
+
+char buf[BUFSIZE]; /* buffer for ungetch */
+int bufp = 0; /* next free position in buf */
+int getch();
+void ungetch();
+
+int sp = 0; /* next free stack position */
+double val[MAXVAL]; /* value stack */
+void push(double val);
+double pop();
+
+/* reverse Polish calculator */
+main()
+{
+    int type;
+    double op2;
+    char s[MAXOP];
+
+    while ((type = getop(s)) != EOF) {
+        switch (type) {
+            case NUMBER:
+                push(atof(s));
+                break;
+            case '+':
+                push(pop() + pop());
+                break;
+            case '*':
+                push(pop() * pop());
+                break;
+            case '-':
+                op2 = pop();
+                push(pop() - op2);
+                break;
+            case '/':
+                op2 = pop();
+                if (op2 != 0.)
+                    push(pop() / op2);
+                else
+                    printf("error: zero divisor\n");
+                break;
+            case '%':
+                op2 = pop();
+                if (op2 != 0.)
+                    push((int)pop() % (int)op2);
+                else
+                    printf("error: zero divisor\n");
+                break;
+            case '\n':
+                printf("\t%.8g\n", pop());
+                break;
+            default:
+                printf("error: unknown command %s\n", s);
+                break;
+        }
+    }
+}
+
+int getop(char s[])
+{
+    int i, c;
+    
+    while ((s[0] = c = getch()) == ' ' || c == '\t')
+        ;
+            
+    s[1] = '\0';
+    if (!isdigit(c) && c != '.' && c != '-')
+        return c; /* not a number */
+    
+    i = 0;
+    if (c == '-')  { /* check if negative number */
+        if (!isdigit(c = getch())) {
+            ungetch(c);
+            return '-';
+        }
+        s[++i] = c;
+    }
+            
+    if (isdigit(c)) /* collect integer part */
+        while (isdigit(s[++i] = c = getch()))
+            ;
+            
+    if (c == '.') /* collect fraction part */
+        while (isdigit(s[++i] = c = getch()))
+            ;
+    
+    s[i] = '\0';
+    if (c != EOF)
+        ungetch(c);
+    
+    return NUMBER;
+}
+
+int getch()
+{
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c) 
+{
+    if (bufp < BUFSIZE)
+        buf[bufp++] = c;
+    else 
+        printf("ungetch: too many characters\n");
+}
+
+void push(double f)
+{
+    if (sp < MAXVAL)
+        val[sp++] = f;
+    else
+        printf("error: stack full, can push %g\n", f);
+}
+
+double pop()
+{
+    if (sp > 0)
+        return val[--sp];
+    else {
+        printf("error: stack empty\n");
+        return 0.;
+    }
+}
