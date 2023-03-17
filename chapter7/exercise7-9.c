935a05c8fa166954031bcdb232b7f65554ab1667
Kelvin van Vuuren <kelvin@kelvinvv.xyz>
2023-03-17 13:44:34 +0000

    Exercise 7-9: fast vs space isupper
    Explore isupper implementations for space saving vs fast execution.
    
    Solution:
    - Slower version uses simple comparison for check within range 'A' -
      'Z'.
    - Fastest utilises a table lookup which is faster due to only having to
      index into preexisting array containing 0 or 1 for each value of char
      c (0...255 and -1 for EOF). However, more space is taken up due to
      storage required for lookup table.

---
+++ chapter7/exercise7-9.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/* Functions like usupper can be implemented to save space or to save time. Explore both possibilites. */
+
+/* Solution:
+ * Slower version uses simple comparison.
+ * Fastest utilises a table lookup which is faster due to only having to index into array. However this takes up more
+space for storage of the table.
+ */
+
+int isupperspace(int c);
+int isuppertime(int c);
+
+int main()
+{
+    char c = 'a';
+
+    isupperspace(c);
+    isuppertime(c);
+}
+
+/* slower but saves on space */
+int isupperspace(int c) { return c >= 'A' && c <= 'Z'; }
+
+/* fastest because it uses a table lookup but takes up more space for storing the table */
+int isuppertime(int c)
+{
+    /* character c can be value of unsigned char (0...255) or EOF (-1) */
+    static const char isupper[257] = {
+
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    };
+
+    return (isupper + 1)[c];
+}
